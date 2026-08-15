73d79e805fcaa63e2e608f11b59df3be16710d2a
hajitoohaji-boop <hajitoohaji@gmail.com>
2026-08-15 12:02:14 +0200

    Export sample comparison tools

--- src/sage_map_builder/analysis/__init__.py
+++ src/sage_map_builder/analysis/__init__.py
@@ -1,12 +1,18 @@
 """Analysis tools for inspecting SAGE map samples without modifying them."""
 
 from .map_probe import MapProbeResult, probe_map
+from .sample_comparison import ByteDifference, compare_bytes
+from .sample_fingerprint import SampleFingerprint, fingerprint
 from .section_scanner import KNOWN_MARKERS, MarkerHit, scan_markers
 
 __all__ = [
+    "ByteDifference",
     "KNOWN_MARKERS",
     "MapProbeResult",
     "MarkerHit",
+    "SampleFingerprint",
+    "compare_bytes",
+    "fingerprint",
     "probe_map",
     "scan_markers",
 ]
