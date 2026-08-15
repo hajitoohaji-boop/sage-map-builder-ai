34cbe4967abd98f5eae0bafe5ab99623c39d89d2
hajitoohaji-boop <hajitoohaji@gmail.com>
2026-08-15 11:02:04 +0200

    Export complete core models

--- src/sage_map_builder/models/__init__.py
+++ src/sage_map_builder/models/__init__.py
@@ -1,5 +1,7 @@
 """Typed domain models."""
 
+from .document import MapDocument
 from .metadata import MapMetadata
+from .world import Vector3, Waypoint, WorldObject
 
-__all__ = ["MapMetadata"]
+__all__ = ["MapDocument", "MapMetadata", "Vector3", "Waypoint", "WorldObject"]
