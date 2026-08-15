0cd6d8a1efbe2b295bc70588bab9e1ae183428bf
hajitoohaji-boop <hajitoohaji@gmail.com>
2026-08-15 12:00:25 +0200

    Export marker index

--- src/sage_map_builder/format/__init__.py
+++ src/sage_map_builder/format/__init__.py
@@ -1,5 +1,15 @@
 """Map-format domain primitives."""
 
+from .map_header import MapHeaderObservation, inspect_prefix
+from .marker_index import MarkerIndex, MarkerLocation, build_marker_index
 from .section import ByteRange, SectionObservation
 
-__all__ = ["ByteRange", "SectionObservation"]
+__all__ = [
+    "ByteRange",
+    "MapHeaderObservation",
+    "MarkerIndex",
+    "MarkerLocation",
+    "SectionObservation",
+    "build_marker_index",
+    "inspect_prefix",
+]
