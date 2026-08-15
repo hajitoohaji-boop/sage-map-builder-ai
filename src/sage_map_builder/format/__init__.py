97792c81df8e6e4c085976b95182fe091f6dcc84
hajitoohaji-boop <hajitoohaji@gmail.com>
2026-08-15 12:05:31 +0200

    Export binary field inspection helper

--- src/sage_map_builder/format/__init__.py
+++ src/sage_map_builder/format/__init__.py
@@ -1,15 +1,18 @@
 """Map-format domain primitives."""
 
+from .binary_cursor import FieldSample, sample_u32_i32_f32
 from .map_header import MapHeaderObservation, inspect_prefix
 from .marker_index import MarkerIndex, MarkerLocation, build_marker_index
 from .section import ByteRange, SectionObservation
 
 __all__ = [
     "ByteRange",
+    "FieldSample",
     "MapHeaderObservation",
     "MarkerIndex",
     "MarkerLocation",
     "SectionObservation",
     "build_marker_index",
     "inspect_prefix",
+    "sample_u32_i32_f32",
 ]
