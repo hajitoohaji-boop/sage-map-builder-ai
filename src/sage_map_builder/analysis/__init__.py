ceea899f67fbb62980a7eaae79d6919a06f4fbcc
hajitoohaji-boop <hajitoohaji@gmail.com>
2026-08-18 15:42:43 +0200

    Export new deterministic binary evidence tools

--- src/sage_map_builder/analysis/__init__.py
+++ src/sage_map_builder/analysis/__init__.py
@@ -1,18 +1,33 @@
 """Analysis tools for inspecting SAGE map samples without modifying them."""
 
+from .chunk_stream_probe import ChunkStreamProbe, find_valid_chunk_streams, probe_chunk_stream
+from .ckmp_evidence import CkMpEvidence, find_ckmp_evidence, first_ckmp_evidence
 from .map_probe import MapProbeResult, probe_map
 from .sample_comparison import ByteDifference, compare_bytes
+from .sample_evidence import SampleEvidence, collect_sample_evidence, sample_evidence_dict
 from .sample_fingerprint import SampleFingerprint, fingerprint
 from .section_scanner import KNOWN_MARKERS, MarkerHit, scan_markers
+from .source_chunk_markers import SourceChunkMarker, find_source_chunk_markers
 
 __all__ = [
     "ByteDifference",
+    "CkMpEvidence",
+    "ChunkStreamProbe",
     "KNOWN_MARKERS",
     "MapProbeResult",
     "MarkerHit",
+    "SampleEvidence",
     "SampleFingerprint",
+    "SourceChunkMarker",
+    "collect_sample_evidence",
     "compare_bytes",
+    "find_ckmp_evidence",
+    "find_source_chunk_markers",
+    "find_valid_chunk_streams",
     "fingerprint",
+    "first_ckmp_evidence",
+    "probe_chunk_stream",
     "probe_map",
+    "sample_evidence_dict",
     "scan_markers",
 ]
